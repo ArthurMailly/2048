71bd99c2685724ce48bbc60f6517c5a854d5313e
Fares <58696751+Tavolata@users.noreply.github.com>
2024-10-01 08:39:45 +0200

    swingtest

--- dmqh/src/main/java/com/ares/Main.java
+++ dmqh/src/main/java/com/ares/Main.java
@@ -1,7 +1,11 @@
 package com.ares;
 
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Dmqh");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
     }
 }

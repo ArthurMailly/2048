c35f4a97969f0bbfd09a31470db8524b3494ca50
Fares <58696751+Tavolata@users.noreply.github.com>
2024-10-01 09:44:45 +0200

    Test

--- dmqh/src/main/java/com/ares/Main.java
+++ dmqh/src/main/java/com/ares/Main.java
@@ -7,5 +7,6 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setVisible(true);
+        System.out.println("Hello, World!");
     }
 }

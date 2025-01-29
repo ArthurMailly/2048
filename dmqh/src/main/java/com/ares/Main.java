87d080acf478b28bc26a529f883da5efd9aac2e5
Fares <58696751+Tavolata@users.noreply.github.com>
2025-01-29 15:43:56 +0100

    nettoyage et login default

--- dmqh/src/main/java/com/ares/Main.java
+++ dmqh/src/main/java/com/ares/Main.java
@@ -14,24 +14,7 @@ public class Main {
     public static void main(String[] args) {
         bdConnection bd = new bdConnection();
         bd.CreateDB();
-        // bdConnection bd = new bdConnection();
-        // ResultSet results = bd.printAllinDB();
-
-        // try {
-        //     while (results.next()) {
-        //         String username = results.getString("username");
-        //         int score = results.getInt("score");
-
-        //         System.out.println(username + " : " + score);
-                
-        //     }
-        // } catch (SQLException e) {
-        //     e.printStackTrace();
-        // }
-
-        // controllerDmqh controller = controllerDmqh.getInstance();
-        // controller.updateScore(0);
-
+        
         MainMenu frame = new MainMenu();
         frame.setVisible(true);
 
